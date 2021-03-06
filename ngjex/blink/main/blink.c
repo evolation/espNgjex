4ceb757cbe4ce7807b15b18067928cdfde7b0620
evolation <evolation@users.noreply.github.com>
2021-03-06 15:28:07 +0100

    Update blink.c

--- ngjex/blink/main/blink.c
+++ ngjex/blink/main/blink.c
@@ -7,15 +7,99 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/task.h"
+
+#include "esp_system.h"
+
 #include "driver/gpio.h"
-#include "sdkconfig.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
 
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define BLINK_GPIO GPIO_NUM_4
+#define PIN_NUM_CS GPIO_NUM_5
+#define TAG "TMC_APP"
+
+static void send_cmd(spi_device_handle_t spi, const uint8_t cmd)
+{
+    esp_err_t ret;
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));       //Zero out the transaction
+    t.length=8;                     //Command is 8 bits
+    t.tx_buffer=&cmd;               //The data is the cmd itself
+    t.user=(void*)0;                //D/C needs to be set to 0
+    ret=spi_device_polling_transmit(spi, &t);  //Transmit!
+    assert(ret==ESP_OK);            //Should have had no issues.
+}
+
+static uint32_t get_id(spi_device_handle_t spi)
+{
+    //get_id cmd
+    send_cmd(spi, 0x04);
+
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));
+    t.length=8*3;
+    t.flags = SPI_TRANS_USE_RXDATA;
+    t.user = (void*)1;
+
+    esp_err_t ret = spi_device_polling_transmit(spi, &t);
+    assert( ret == ESP_OK );
+
+    return *(uint32_t*)t.rx_data;
+}
+
+void spi_main_lcd(void)
+{
+    esp_err_t ret;
+    spi_device_handle_t spi;
+    
+    const spi_bus_config_t bus_config = {
+        .mosi_io_num = VSPI_IOMUX_PIN_NUM_MOSI,
+        .miso_io_num = VSPI_IOMUX_PIN_NUM_MISO,
+        .sclk_io_num = VSPI_IOMUX_PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+    };
+
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=26*1000*1000,           //Clock out at 10 MHz
+        .mode=0,                                //SPI mode 0
+        .spics_io_num=PIN_NUM_CS,               //CS pin
+        .queue_size=7                          //We want to be able to queue 7 transactions at a time
+    };
+
+   // Initialize the SPI bus
+    ESP_ERROR_CHECK(spi_bus_initialize(VSPI_HOST, &bus_config, 1));
+
+    //Attach the LCD to the SPI bus
+    ret=spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
+    //Initialize the LCD
+
+    ESP_LOGI(TAG, "Initializing external SPI");
+    ESP_LOGI(TAG, "Pin assignments:");
+    ESP_LOGI(TAG, "MOSI: %2d   MISO: %2d   SCLK: %2d   CS: %2d",
+        bus_config.mosi_io_num, bus_config.miso_io_num,
+        bus_config.sclk_io_num, devcfg.spics_io_num
+    );
+
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Request ID -> %d :",get_id(spi));
+        
+    }
+    
+}
 
 void app_main(void)
 {
@@ -25,6 +109,8 @@ void app_main(void)
        Technical Reference for a list of pads and their default
        functions.)
     */
+    xTaskCreate(spi_main_lcd,"spi_main_task",10000,NULL,tskIDLE_PRIORITY+1,NULL);
+
     gpio_reset_pin(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
@@ -35,7 +121,8 @@ void app_main(void)
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
         printf("Turning on the LED\n");
-        gpio_set_level(BLINK_GPIO, 1);
+        gpio_set_level(BLINK_GPIO, 1);    
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
+
 }
